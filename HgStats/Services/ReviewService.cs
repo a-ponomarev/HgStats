de6dc052ae59f5942bf08c6dccc385e0630363c1
a-ponomarev <aponomarev.ekb@gmail.com>
2018-10-01 20:20:29 +0300

    Allow maps absence

--- HgStats/Services/ReviewService.cs
+++ HgStats/Services/ReviewService.cs
@@ -18,19 +18,37 @@ namespace HgStats.Services
         private readonly Dictionary<string, string> authorMap;
         private readonly Dictionary<string, string> reviewerMap;
         private readonly HashSet<string> authors;
+        private string AuthorMapPath => Path.Combine(hgRoot, "authormap.txt");
+        private string ReviewerMapPath => Path.Combine(hgRoot, "reviewermap.txt");
         private string CD => $"cd {hgRoot}";
 
         public ReviewService()
         {
             hgRoot = new SettingsService().Settings.HgRoot;
-            authorMap = File.ReadAllLines(Path.Combine(hgRoot, "authormap.txt"))
-                      .Select(l => l.Split('='))
-                      .ToDictionary(x => x[0], x => x[1]);
-            reviewerMap = File.ReadAllLines(Path.Combine(hgRoot, "reviewermap.txt"))
-                              .Select(l => l.Split('='))
-                              .SelectMany(x => x[0].Split(',').Select(r => new { key = r, val = x[1] }))
-                              .ToDictionary(x => x.key, x => x.val);
+            authorMap = GetAuthorMap();
+            reviewerMap = GetReviewerMap();
             authors = GetAuthors();
+        }
+
+        private Dictionary<string, string> GetAuthorMap()
+        {
+            if (!File.Exists(AuthorMapPath))
+                return new Dictionary<string, string>();
+
+            return File.ReadAllLines(AuthorMapPath)
+                .Select(l => l.Split('='))
+                .ToDictionary(x => x[0], x => x[1]);
+        }
+
+        private Dictionary<string, string> GetReviewerMap()
+        {
+            if (!File.Exists(ReviewerMapPath))
+                return new Dictionary<string, string>();
+
+            return File.ReadAllLines(ReviewerMapPath)
+                .Select(l => l.Split('='))
+                .SelectMany(x => x[0].Split(',').Select(r => new { key = r, val = x[1] }))
+                .ToDictionary(x => x.key, x => x.val);
         }
 
         public string GetData(string from, string to)
