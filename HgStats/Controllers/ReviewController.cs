5cde8ee80c1c0c087e907fb4262df7e6fae3ae7d
a-ponomarev <aponomarev.ekb@gmail.com>
2018-10-20 01:19:12 +0300

    Total counts

--- HgStats/Controllers/ReviewController.cs
+++ HgStats/Controllers/ReviewController.cs
@@ -16,11 +16,18 @@ namespace HgStats.Controllers
         public ActionResult Data(string from, string to)
         {
             var header = $"root,author,review,amount{Environment.NewLine}";
+            var lines = GetData(from, to).Select(d => $"{d.root},{d.author},{d.reviewer},{d.count}");
 
-            var data = ReviewServiceCollection.Services.SelectMany(s =>
-                s.GetData(from, to).Select(d => $"{d.root},{d.author},{d.reviewer},{d.count}"));
+            return Content(header + string.Join(Environment.NewLine, lines));
+        }
 
-            return Content(header + string.Join(Environment.NewLine, data));
+        private static IEnumerable<(string root, string author, string reviewer, int count)> GetData(string @from, string to)
+        {
+            var rawData = ReviewServiceCollection.Services.SelectMany(s => s.GetData(from, to)).ToArray();
+            var total = rawData.GroupBy(d => (d.author, d.reviewer)).Select(g =>
+                (root: "Total", g.Key.author, g.Key.reviewer, count: g.Sum(r => r.count)));
+
+            return total.Concat(rawData);
         }
     }
 }
