5feb618a17578720cc2b9c4b3fa9b9595af9060e
a-ponomarev <aponomarev.ekb@gmail.com>
2018-10-22 01:49:16 +0300

    Read settings

--- reviewService.js
+++ reviewService.js
@@ -1,12 +1,11 @@
+const settings = require('./settings.json');
+
 exports.getData = (from, to) => {
-   return [
-       {root: 'C:\\OneRepo', author:'a', review: 'b', amount: 10},
-       {root: 'C:\\OneRepo', author:'a', review: 'c', amount: 5},
-       {root: 'C:\\OneRepo', author:'a', review: 'd', amount: 6},
-       {root: 'C:\\OneRepo', author:'c', review: 'a', amount: 3},
-       {root: 'C:\\OneRepo', author:'c', review: 'b', amount: 2},
-       {root: 'C:\\OtherRepo', author:'a', review: 'c', amount: 5},
-       {root: 'C:\\OtherRepo', author:'a', review: 'd', amount: 6},
-       {root: 'C:\\OtherRepo', author:'c', review: 'a', amount: 3}
-   ]
-}
+    return settings.hgRoots.map(root => [
+       {root: root, author:'a', review: 'b', amount: 10},
+       {root: root, author:'a', review: 'c', amount: 5},
+       {root: root, author:'a', review: 'd', amount: 6},
+       {root: root, author:'c', review: 'a', amount: 3},
+       {root: root, author:'c', review: 'b', amount: 2}
+   ]).reduce((a,b) => a.concat(b));
+};
